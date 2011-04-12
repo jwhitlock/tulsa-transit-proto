8350b32192bb45758cb44d5d3a166a0bd86868e7
John Whitlock <John-Whitlock@ieee.org>
2011-04-11 19:12:14 -0500

    Working map

--- tulsa-transit-proto/views/stop.py
+++ tulsa-transit-proto/views/stop.py
@@ -5,8 +5,13 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
-
 class View(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'templates', 'stop.html')
+        submission = False
+        self.response.out.write(template.render(path, locals()))
+        
+    def post(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates', 'stop.html')
+        submission = True
         self.response.out.write(template.render(path, locals()))
